b7d0b4c168a3cc02b668ef3031b0cfd42b725da5
Fernando Cruz <f.cruzval@gmail.com>
2017-01-18 12:58:15 -0600

    CRUD actions for posts, categories and comments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def fix_url(url_str)
+    url_str.starts_with?('http://') ? url_str : "http://#{url_str}"
+  end
+
+  def display_datetime(date)
+    "#{time_ago_in_words(date)} ago."
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 PostitTemplate::Application.routes.draw do
   root to: 'posts#index'
 
-  resources :posts, except: :destroy
+  resources :posts, except: :destroy do
+    # nested resoruce /posts/3/comments
+    resources :comments, only: :create
+  end
 
+  resources :categories, only: [:show, :new, :create]
 end
